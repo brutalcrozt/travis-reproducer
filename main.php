f551d6ee0b40cf67607981e5177e927ff7a1eb2d
Adi Prasetyo <goendoell49@gmail.com>
2017-09-03 06:35:38 +0700

    Update main.php

--- main.php
+++ main.php
@@ -1,15 +1,15 @@
 <?php 
-  echo "you on main.php";
+  echo "you on main.php". PHP_EOL;
   require_once ".\vendor\autoload.php";
   use mikehaertl\shellcommand\Command;
 
   // Basic example
   $command = new Command('/usr/local/bin/mycommand -a -b');
   if ($command->execute()) {
-    echo "from output\n";
+    echo "from output". PHP_EOL;
     echo $command->getOutput();
   } else {
-    echo "from err\n";
+    echo "from err". PHP_EOL;
     echo $command->getError();
     $exitCode = $command->getExitCode();
     echo "exitCode: ". $exitCode;
