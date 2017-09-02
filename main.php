3733bcc004daec46e008423c147a1d000fe3e707
Adi Prasetyo <goendoell49@gmail.com>
2017-09-03 06:23:05 +0700

    Update main.php

--- main.php
+++ main.php
@@ -1,3 +1,16 @@
 <?php 
-  echo "you on main.php"
+  echo "you on main.php";
+  use mikehaertl\shellcommand\Command;
+
+  // Basic example
+  $command = new Command('/usr/local/bin/mycommand -a -b');
+  if ($command->execute()) {
+    echo "from output\n";
+    echo $command->getOutput();
+  } else {
+    echo "from err\n";
+    echo $command->getError();
+    $exitCode = $command->getExitCode();
+    echo "exitCode: ". $exitCode;
+  }
 ?>
